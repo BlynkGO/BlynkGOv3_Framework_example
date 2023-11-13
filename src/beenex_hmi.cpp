60c350977b6c2419059f7b522834fd1c5697c7f4
BlynkGO <prote2000@gmail.com>
2023-11-14 02:53:48 +0700

    Update beenex_hmi.cpp

--- src/beenex_hmi.cpp
+++ src/beenex_hmi.cpp
@@ -1,22 +1,22 @@
 #include "project.h"
 
-void BEENEXT_HMI::send(String key, bool state) {
+inline void BEENEXT_HMI::send(String key, bool state) {
   BeeNeXT.send(key, state);
 }
 
-void BEENEXT_HMI::send(String key, int num) {
+inline void BEENEXT_HMI::send(String key, int num) {
   BeeNeXT.send(key, num);
 }
 
-void BEENEXT_HMI::send(String key, float f) {
+inline void BEENEXT_HMI::send(String key, float f) {
   BeeNeXT.send(key, f);
 }
 
-void BEENEXT_HMI::send(String key, String str) {
+inline void BEENEXT_HMI::send(String key, String str) {
   BeeNeXT.send(key, str);
 }
 
-void BEENEXT_HMI::send(String key, const char* str) {
+inline void BEENEXT_HMI::send(String key, const char* str) {
   BeeNeXT.send(key, str);
 }
 
